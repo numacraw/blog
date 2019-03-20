1c09ab02f94443ee68b32810c73e261547cd8b4b
liusingxing <qq172105117@gmail.com>
2019-03-20 16:48:17 +0800

    feat: init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Index from "./views/home/index";
+
+class App extends Component {
+  render() {
+    return <Index />;
+  }
+}
+
+export default App;
